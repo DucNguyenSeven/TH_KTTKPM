edc36bc41dc3d43b82cfaebe56d0774a8f0a502e
nguyenduc <ngduc0106@gmail.com>
2025-04-12 07:14:41 +0700

    git add . git commit -m "feat(product): implement CRUD API for product service"

---
+++ TH_12_04_2025/product-service/src/main/java/ngduc/productservice/controller/ProductController.java
@@ -0,0 +1,43 @@
+package ngduc.productservice.controller;
+
+import ngduc.productservice.model.Product;
+import ngduc.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return ResponseEntity.ok(service.save(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getOne(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.ok(service.update(id, product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ TH_12_04_2025/product-service/src/main/java/ngduc/productservice/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package ngduc.productservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
